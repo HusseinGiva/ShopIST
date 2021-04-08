be23a0334d48f9f7854d4c241c3ca2ae4c1f2618
HusseinGiva <hussein.giva@tecnico.ulisboa.pt>
2021-04-08 13:51:01 +0100

    Added register activity and linked to main login activity

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/RegisterActivity.java
@@ -0,0 +1,15 @@
+package pt.ulisboa.tecnico.cmov.shopist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
