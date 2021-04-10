b12688b12984fca49783adaa8475f8db684e425a
HusseinGiva <hussein.giva@tecnico.ulisboa.pt>
2021-04-10 19:20:37 +0100

    Corrected errors and removed TERMITE

--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
@@ -8,10 +8,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import pt.inesc.termite.wifidirect.SimWifiP2pBroadcast;
-import pt.inesc.termite.wifidirect.SimWifiP2pManager;
-import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocket;
-import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocketServer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,12 +18,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.mainToolbar);
         setSupportActionBar(myToolbar);
 
-        //Termite Test - DELETE LATER
-        SimWifiP2pBroadcast a = new SimWifiP2pBroadcast();
-        SimWifiP2pManager mManager = null;
-        SimWifiP2pManager.Channel mChanell = null;
-        SimWifiP2pSocketServer mSrvSocket = null;
-        SimWifiP2pSocket mCliSocket = null;
     }
 
     public void onClickSignUp(View view) {
