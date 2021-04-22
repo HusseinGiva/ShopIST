c8c78c65a21c2c15fddd898b400dd24dc9b30ab4
JGMF99 <48356138+JGMF99@users.noreply.github.com>
2021-04-22 12:56:15 +0100

    Login and Register Done

--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
@@ -1,21 +1,63 @@
 package pt.ulisboa.tecnico.cmov.shopist;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/*import pt.inesc.termite.wifidirect.SimWifiP2pBroadcast;
+import pt.inesc.termite.wifidirect.SimWifiP2pManager;
+import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocket;
+import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocketServer;*/
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MAIN" ;
+    private FirebaseAuth mAuth;
+
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = findViewById(R.id.mainToolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.mainToolbar);
         setSupportActionBar(myToolbar);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        //Termite Test - DELETE LATER
+        /*SimWifiP2pBroadcast a = new SimWifiP2pBroadcast();
+        SimWifiP2pManager mManager = null;
+        SimWifiP2pManager.Channel mChanell = null;
+        SimWifiP2pSocketServer mSrvSocket = null;
+        SimWifiP2pSocket mCliSocket = null;*/
+
+        email =  (EditText) findViewById(R.id.emailTextBox);
+        password =  (EditText) findViewById(R.id.passwordTextBox);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
     public void onClickSignUp(View view) {
@@ -24,8 +66,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickNoAccount(View view) {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-        finish();
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInAnonymously:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Something went wrong. Please make sure you have Internet Connection.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    public void onClickLogin(View view) {
+
+        String emailText = email.getText().toString();
+        String passwordText = password.getText().toString();
+
+        //Check if fields are empty
+        if(emailText.trim().isEmpty() || passwordText.trim().isEmpty()){
+            Toast.makeText(this, "Please fill in the required fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null){
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/RegisterActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/RegisterActivity.java
@@ -1,12 +1,42 @@
 package pt.ulisboa.tecnico.cmov.shopist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "REGISTER";
+    private EditText email;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText password;
+    private EditText confirmPassword;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +47,117 @@ public class RegisterActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         assert ab != null;
         ab.setDisplayHomeAsUpEnabled(true);
+
+        email =  (EditText) findViewById(R.id.registerEmailTextBox);
+        firstName =  (EditText) findViewById(R.id.firstNameTextBox);
+        lastName =  (EditText) findViewById(R.id.lastNameTextBox);
+        password =  (EditText) findViewById(R.id.registerPasswordTextBox);
+        confirmPassword =  (EditText) findViewById(R.id.confirmPasswordTextBox);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Access a Cloud Firestore instance from your Activity
+        db = FirebaseFirestore.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void OnClickRegister(View view) {
+
+        String emailText = email.getText().toString();
+        String firstNameText = firstName.getText().toString();
+        String lastNameText = lastName.getText().toString();
+        String passwordText = password.getText().toString();
+        String confirmPasswordText = confirmPassword.getText().toString();
+
+        //Check if name fields are empty
+        if(firstNameText.trim().isEmpty() || lastNameText.trim().isEmpty()){
+            Toast.makeText(this, "Invalid name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Check if email is valid
+        if(!isValidEmail(emailText)){
+            Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Check if password field is empty
+        if(passwordText.trim().isEmpty()){
+            Toast.makeText(this, "Invalid password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Check if passwords match
+        if(!passwordText.equals(confirmPasswordText)){
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            addUserToFirestore(user, firstNameText, lastNameText);
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+    private void addUserToFirestore(FirebaseUser user, String firstNameText, String lastNameText) {
+
+        Map<String, String> userInfo = new HashMap<>();
+
+        userInfo.put("firstName", firstNameText);
+        userInfo.put("lastName", lastNameText);
+
+        db.collection("user").document(user.getUid())
+                .set(userInfo)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null){
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+
+
+    public static boolean isValidEmail(CharSequence target) {
+        return target != null && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+
 }
